f319dedebb06794b420065aade400f98e5f7bce8
Akshay Bengani <akshaybengani@gmail.com>
2018-12-30 18:44:53 +0530

    Created a new Project

---
+++ app/src/main/java/com/akshaybengani/baatcheet/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.akshaybengani.baatcheet;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
+
+
+
+}
